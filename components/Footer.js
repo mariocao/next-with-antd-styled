0fc88ef8935a5bf99e31ba839f7f971027cb7af6
Mario Cao <mariocao@gmail.com>
2018-03-03 20:49:02 +0100

    [ADD] Add footer content

--- components/Footer.js
+++ components/Footer.js
@@ -2,11 +2,21 @@ import styled from "styled-components";
 
 const Footer = styled.div`
   grid-area: footer;
-  background: gray;
+  background: #555;
+`;
+
+const FooterText = styled.p`
+  text-align: center;
+  color: #ddd;
+  font-family: "Avenir Roman", "proxima-nova", "Helvetica Neue", Helvetica,
+    Arial, sans-serif;
+  font-size: 14px;
+  line-height: 18px;
+  margin: 8px;
 `;
 
 export default ({ children }) => (
   <Footer>
-    <p>Footer</p>
+    <FooterText>2018 Mario Cao</FooterText>
   </Footer>
 );
