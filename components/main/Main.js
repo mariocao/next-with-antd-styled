d6033391ebf5d1b5f4b5645cb185166d3882b779
Mario Cao <mariocao@gmail.com>
2018-03-03 18:21:09 +0100

    [ADD] Main page sample

---
+++ components/main/Main.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import Features from "./Features";
+import Techs from "./Techs";
+import SampleForm from "./SampleForm";
+
+const BackgroundImage = styled.div`
+  padding: 20px;
+  background: url(static/bg.jpg) no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+`;
+
+const Title = styled.h1`
+  padding: 20px;
+  text-align: center;
+  font-size: 64px;
+  line-height: 74px;
+  text-shadow: 0px 1px 3px rgba(0, 0, 0, 0.25);
+  color: #fff;
+  font-family: "Flama Light", "proxima-nova", "Helvetica Neue", Helvetica, Arial,
+    sans-serif;
+  font-weight: 100;
+`;
+
+class Main extends React.Component {
+  render() {
+    return (
+      <BackgroundImage>
+        <Title>
+          Welcome to our<br />Landing page!
+        </Title>
+        <SampleForm />
+      </BackgroundImage>
+    );
+  }
+}
+
+export default Main;
---
+++ components/main/SampleForm.js
@@ -0,0 +1,97 @@
+import { Form, Input, Icon, Checkbox, Button } from "antd";
+import styled from "styled-components";
+
+const FormItem = Form.Item;
+
+const Subtitle = styled.h2`
+  padding: 20px;
+  text-align: center;
+  font-size: 26px;
+  line-height: 32px;
+  text-shadow: 0px 1px 3px rgba(0,0,0,0.25);
+    color: #fff;
+  font-family: "Flama Light", "proxima-nova", "Helvetica Neue", Helvetica, Arial,
+    sans-serif;
+  font-weight: 100;
+`;
+
+class SampleForm extends React.Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 12,
+          offset: 6
+        },
+        sm: {
+          span: 12,
+          offset: 6
+        },
+        md: {
+          span: 8,
+          offset: 8
+        },
+        lg: {
+          span: 6,
+          offset: 9
+        }
+      }
+    };
+
+    return (
+      <section>
+        <Subtitle>Request an early access:</Subtitle>
+        <Form onSubmit={this.handleSubmit}>
+          <FormItem {...formItemLayout}>
+            {getFieldDecorator("email", {
+              rules: [
+                {
+                  type: "email",
+                  message: "The input is not valid E-mail!"
+                },
+                {
+                  required: true,
+                  message: "Please input your E-mail!"
+                }
+              ]
+            })(
+              <Input
+                prefix={
+                  <Icon type="mail" style={{ color: "rgba(0,0,0,.25)" }} />
+                }
+                placeholder="your@email.com"
+              />
+            )}
+          </FormItem>
+          <FormItem {...formItemLayout}>
+            {getFieldDecorator("agreement", {
+              valuePropName: "checked"
+            })(
+              <Checkbox style={{ color: "#FFF" }}>
+                I have read the <a href="">agreement</a>
+              </Checkbox>
+            )}
+          </FormItem>
+          <FormItem {...formItemLayout}>
+            <Button style={{}} type="primary" htmlType="submit">
+              Send
+            </Button>
+          </FormItem>
+        </Form>
+      </section>
+    );
+  }
+}
+
+const WrappedRegistrationForm = Form.create()(SampleForm);
+export default WrappedRegistrationForm;
